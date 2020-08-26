30edf8510f25e89d6f904341350b192d782cadc8
Djellel Difallah <difallah@gmail.com>
2020-08-26 13:03:48 +0200

    Add multi-language support
    Add multi-language support
    Output a shelved dictionary along with a pickle
    Solve the issue with the first capital letter of a link

--- scripts/generate_anchor_dictionary.py
+++ scripts/generate_anchor_dictionary.py
@@ -1,36 +1,72 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import xml.sax
+import sys, os
 import mwparserfromhell
 import bz2, subprocess
 import pickle
+import shelve
 import wikitextparser as wtp
-from tqdm import trange, tqdm
 import glob
+import mwxml
+
+from tqdm import trange, tqdm
 from collections import Counter
 
-
-import mwxml
 
 # Full latest-articles dump
 # This path is for WMF cluster
 # Download the dumps otherwise
-paths = glob.glob('/mnt/data/xmldatadumps/public/enwiki/latest/enwiki-latest-pages-articles*.xml*.bz2')
 
+####################
+paths = []
 
+if len(sys.argv) >= 2:
+    lang = sys.argv[1]
+else:
+    lang = 'en'
+
+wiki   = lang+'wiki'
+
+dirpath = '/mnt/data/xmldatadumps/public/{0}/*'.format(wiki)
+
+# Get the penultimate dump directory (the dir "latest" can have some simlink issues")
+try:
+    files = glob.glob(dirpath)
+    files.sort(key=os.path.getmtime)
+    for f in files:
+        if 'latest' in f:
+            files.remove(f)
+            break
+    snapshot = files[-1].split('/')[-1]
+except:
+    snapshot = 'latest'
+    
+dump_fn = '/mnt/data/xmldatadumps/public/{0}/{1}/{0}-{1}-pages-articles.xml.bz2'.format(wiki,snapshot)
+for infile in glob.glob('/mnt/data/xmldatadumps/public/{0}/{1}/{0}-{1}-pages-articles*.xml*.bz2'.format(wiki,snapshot) ):
+    if infile == dump_fn:
+        continue
+    if 'multistream' in infile:
+        continue
+    paths += [infile]
+if len(paths) == 0:
+    paths+=[dump_fn]
+
+print("Processing the following Wikipedia dump files:")
+for p in paths:
+    print(p)
 
 ####################
 # There are a number of todos here
 
-# TODO 1: add a parameter LANG to process any language
-
-# TODO 2: generate a list containing the title of all redirects and disambiguation
+# TODO 1: generate a list containing the title of all redirects and disambiguation
 # save the list in a pickle to reuse it.
 # Maybe, it's good to discard any recommendation pointing to a redirect/disambiguation
 
-# TODO 3: extract the page_id, page_title) mapping file
+# TODO 2: extract the (page_id, page_title) mapping file
 # It seems that the navigation embeddings are indexed on page_id
+
+# TODO 3: deal with special links (categories, files, etc.)
 
 
 
@@ -58,8 +94,6 @@ paths = glob.glob('/mnt/data/xmldatadumps/public/enwiki/latest/enwiki-latest-pag
 
 anchors = {}
 
-
-
 ##################
 # dump iteration on each page
 # from each page with extract pairs of (link, text)
@@ -67,14 +101,19 @@ def process_dump(dump, path):
     for page in dump:
         if page.redirect or page.namespace != 0:
             continue
-        linklist = wtp.parse(next(page).text).wikilinks
+        try:
+            linklist = wtp.parse(next(page).text).wikilinks
+        except:
+            # TODO: log the exception. I've seen some encoding errors.
+            continue
         for l in linklist:
             try:
-                link = l.title.strip().replace('_', ' ')
-                text = l.text.strip() if l.text else link
-                #print(">>>", text, link, l)
+                link_tmp = l.title.strip().replace('_', ' ')
+                link = link_tmp[0].upper() + link_tmp[1:]
+                text = l.text.strip() if l.text else link_tmp
                 yield link, text
             except:
+                # TODO: log the exception. I've seen some encoding errors.
                 continue
     print("Done processing path:", path)
 
@@ -97,12 +136,22 @@ print("Number of anchors", len(anchors))
 
 ##################
 # turn redundant pairs into Count
-# e.g., ['wmf', 'wmf', 'wmf'] -> 'wmf':3
+# e.g., ['wmf', 'wmf', 'wmf'] -> {'wmf':3}
 print("Compressing the dictionary")
 for k,v in tqdm(anchors.items()):
     anchors[k] = dict(Counter(v))
 
 ##################
+output_path = '../data/{0}/{1}.{0}.anchors'.format(lang, snapshot)
+
+print("Storing the dictionary as Shelve")
+# shelve file will have an additional .db extension
+shelf_db = shelve.open(output_path)
+for k,v in tqdm(anchors.items()):
+    shelf_db[k] = v
+shelf_db.close()
+
+##################
 # store the dictionary into the language data folder
-with open('../data/en/en.anchors.pkl', 'wb') as handle:
+with open(output_path+'.pkl', 'wb') as handle:
     pickle.dump(anchors, handle, protocol=pickle.HIGHEST_PROTOCOL)
