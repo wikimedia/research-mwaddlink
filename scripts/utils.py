f3031881dbabb6437e25cf792fbada6f00c5f5c8
Martin Gerlach <mgerlach@stat1005.eqiad.wmnet>
2020-09-09 07:33:56 +0000

    get_feature_set into utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,5 +1,9 @@
 import urllib
 import urllib.parse as up
+import numpy as np
+
+######################
+## parsing titles
 def normalise_title(title):
     """ 
     Normalising title (links)
@@ -29,3 +33,65 @@ def normalise_anchor(anchor):
     return n_anchor.lower()
 
 
+def wtpGetLinkAnchor(wikilink):
+    '''
+    extract anchor and link from a wikilink from wikitextparser.
+    normalise title and anchor
+    '''
+    ## normalise the article title (quote, first letter capital) 
+    link_tmp = wikilink.title
+    link = normalise_title(link_tmp)
+    ## normalise the anchor text (strip and lowercase) 
+    anchor_tmp = wikilink.text if wikilink.text else link_tmp
+    anchor = normalise_anchor(anchor_tmp)
+    return link, anchor
+
+##########################
+## getting feature-dataset
+##########################
+from scipy.stats import kurtosis
+from Levenshtein import distance as levenshtein_distance
+
+# Wikipedia2Vec distance
+def getW2VDst(ent_a, ent_b, word2vec):
+    dst = 0
+    try:
+        a = word2vec.get_entity_vector(ent_a)
+        b = word2vec.get_entity_vector(ent_b)
+        dst = (np.dot(a, b) / np.linalg.norm(a) / np.linalg.norm(b))
+    except:
+        pass
+    if np.isnan(dst):
+        dst=0
+    return dst
+
+# Navigation distance
+# TODO: the navigation model should change to (page_title, vector)
+# This piece won't be needed then
+# Latest update: we might keep page_id because it's much easier for Nav2vec
+
+def getNavDst(ent_a, ent_b, nav2vec, pageids):
+    '''
+    the entities are strings (even if it is a page-id)
+    '''
+    dst = 0
+    try:
+        page_a = str(pageids[ent_a])
+        page_b = str(pageids[ent_b])
+        a = nav2vec.get_word_vector(page_a)
+        b = nav2vec.get_word_vector(page_b)
+        dst = (np.dot(a, b) / np.linalg.norm(a) / np.linalg.norm(b))
+    except:
+        pass
+    if np.isnan(dst):
+        dst=0
+    return dst
+# Return the features for each link candidate in the context of the text and the page
+def get_feature_set(page, text, link, anchors, word2vec, nav2vec, pageids):
+    ngram = len(text.split()) # simple space based tokenizer to compute n-grams
+    freq = anchors[text][link] # How many times was the link use with this text 
+    ambig = len(anchors[text]) # home many different links where used with this text
+    kur = kurtosis(sorted(list(anchors[text].values()), reverse = True) + [1] * (1000 - ambig)) # Skew of usage text/link distribution
+    w2v = getW2VDst(page, link,word2vec) # W2V Distance between the source and target page
+    nav = getNavDst(page, link, nav2vec, pageids) # Nav Distance between the source and target page
+    return (ngram, freq, ambig, kur, w2v, nav)
