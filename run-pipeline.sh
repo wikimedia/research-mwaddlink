131b4a41f9a884db0fb77b953595ddd6c0b2699f
Martin Gerlach <mgerlach@wikimedia.org>
2020-09-25 17:01:46 +0000

    inlcude backtesing to running the pipeline

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -25,9 +25,14 @@ source /usr/lib/anaconda-wmf/bin/activate
 PYSPARK_PYTHON=python3.7 PYSPARK_DRIVER_PYTHON=python3.7 spark2-submit --master yarn --executor-memory 8G --executor-cores 4 --driver-memory 2G  generate_anchor_dictionary_spark.py $LANG
 conda deactivate
 
+# alternatively, one can get the anchor-dictionary by processing the xml-dumps
+# note that this does not filter by link-probability
+# source ~/venv_custom/mwaddlink/bin/activate
+# python generate_anchor_dictionary.py $LANG
+
 source ~/venv_custom/mwaddlink/bin/activate
 
-# get wikipedia2vec-mebddingq
+get wikipedia2vec-mebddingq
 echo 'RUNNING wikipedia2vec on dump'
 wikipedia2vec train --min-entity-count=0 --dim-size 50 --pool-size 10 "/mnt/data/xmldatadumps/public/"$LANG"wiki/latest/"$LANG"wiki-latest-pages-articles.xml.bz2" "../data/"$LANG"/"$LANG".w2v.bin"
 
@@ -47,9 +52,11 @@ python generate_shelve_data.py $LANG
 # # turn into features
 echo 'GENERATING FEATURES'
 python generate_training_data.py $LANG
-# # 
+
+#  train model
 echo 'TRAINING THE MODEL'
 python generate_addlink_model.py $LANG
 
-## TODO
-## add backtesting
+# ## perform automatic backtesting
+echo 'RUNNING BACKTESTING EVALUATION'
+python generate_backtesting_eval.py -l $LANG -nmax 10000
