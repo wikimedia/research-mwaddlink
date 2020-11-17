c6737b5089876227e8009ba91b6a29a9b5ef3086
Kosta Harlan <kharlan@wikimedia.org>
2020-11-17 12:26:39 +0100

    Add Swagger for API documentation
    * Reroute / to apidocs
    * Define validation file for input and provide structured info about
    output
    
    Bug: T266540
    Change-Id: I25fd7611668bd56d3e474aa6774e35f20ee78597

--- api.py
+++ api.py
@@ -1,15 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, redirect
+from flasgger import Swagger, swag_from, validate
 import json_logging
 import logging
 import os
 from sys import stdout
 from src.DatasetLoader import DatasetLoader
 from src.query import Query
-from webargs import fields
-from webargs.flaskparser import parser
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+swag = Swagger(app)
 json_logging.init_flask(enable_json=True)
 json_logging.init_request_instrument(app)
 logger = logging.getLogger("logger")
@@ -21,29 +21,25 @@ load_dotenv()
 
 @app.route("/", methods=["GET"])
 def main():
-    return "TODO: Some landing page or API docs."
+    return redirect("/apidocs")
 
 
+@swag_from("linkrecommendation.yml")
 @app.route("/query", methods=["POST"])
 def query():
-    query_args = {
-        "wikitext": fields.Str(required=True),
-        "revid": fields.Int(required=True),
-        "pageid": fields.Int(required=True),
-        "threshold": fields.Float(required=True),
-        "wiki_id": fields.Str(required=True),
-        "page_title": fields.Str(required=True),
-    }
-    args = parser.parse(query_args, request)
+    data = request.json
+    validate(data, "Input", "linkrecommendation.yml")
     datasetloader = DatasetLoader(
-        backend=os.environ.get("DB_BACKEND"), wiki_id=args["wiki_id"]
+        backend=os.environ.get("DB_BACKEND"), wiki_id=data["wiki_id"]
     )
     query_instance = Query(logger, datasetloader)
-    return query_instance.run(
-        wikitext=args["wikitext"],
-        revid=args["revid"],
-        pageid=args["pageid"],
-        threshold=args["threshold"],
-        wiki_id=args["wiki_id"],
-        page_title=args["page_title"],
+    return jsonify(
+        query_instance.run(
+            wikitext=data["wikitext"],
+            revid=data["revid"],
+            pageid=data["pageid"],
+            threshold=data["threshold"],
+            wiki_id=data["wiki_id"],
+            page_title=data["page_title"],
+        )
     )
