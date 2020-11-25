52fbbf20a36db138029299bd0537e0e75ac8e401
Martin Gerlach <mgerlach@wikimedia.org>
2020-11-25 11:56:11 +0000

    Add read_default_file as additional argument for MySQL access
    Example usage:
    
    DB_USER=user DB_DATABASE=addlink DB_READ_DEFAULT_FILE=config.cnf DB_HOST=host DB_PORT=port python copy-sqlite-to-mysql.py --help
    
    Bug: T265610
    Change-Id: Ie452e9a3afb6eb237c41ab64eafce85067207132

--- src/mysql.py
+++ src/mysql.py
@@ -13,6 +13,7 @@ def get_mysql_connection():
         host=connection_dict["host"],
         database=connection_dict["database"],
         port=connection_dict["port"],
+        read_default_file=connection_dict["read_default_file"],
         charset=connection_dict["charset"],
         use_unicode=connection_dict["use_unicode"],
     )
@@ -25,6 +26,7 @@ def get_connection_dict() -> dict:
         "host": os.environ.get("DB_HOST"),
         "database": os.environ.get("DB_DATABASE"),
         "port": int(os.environ.get("DB_PORT", 3306)),
+        "read_default_file": os.environ.get("DB_READ_DEFAULT_FILE"),
         "charset": "utf8",
         "use_unicode": True,
     }
