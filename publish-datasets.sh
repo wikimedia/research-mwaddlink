440c92efac89e110e56ed4c9c1f0628e09d9c6df
Gergő Tisza <tgr.huwiki@gmail.com>
2021-01-26 03:28:06 -0800

    Fix glob escaping in publish-datasets.sh
    Change-Id: Iccd5caf006947b7a9ce228e2b80cc94eee90a47e

--- publish-datasets.sh
+++ publish-datasets.sh
@@ -7,10 +7,10 @@ DATASET_PATH=$(pwd)/data/${WIKI_ID}
 
 DATASET_PUBLISH_PATH="/srv/published/datasets/one-off/research-mwaddlink/$WIKI_ID"
 mkdir -p "$DATASET_PUBLISH_PATH"
-cp "$DATASET_PATH/*.sql.gz" "$DATASET_PUBLISH_PATH/"
-cp "$DATASET_PATH/*.sqlite.gz" "$DATASET_PUBLISH_PATH/"
+cp "$DATASET_PATH/"*.sql.gz "$DATASET_PUBLISH_PATH/"
+cp "$DATASET_PATH/"*.sqlite.gz "$DATASET_PUBLISH_PATH/"
 cp "$DATASET_PATH/$WIKI_ID.linkmodel.json" "$DATASET_PUBLISH_PATH/"
-cp "$DATASET_PATH/*.checksum" "$DATASET_PUBLISH_PATH/"
+cp "$DATASET_PATH/"*.checksum "$DATASET_PUBLISH_PATH/"
 
 echo "These datasets can be used with Wikimedia's Link Recommendation Service.
 See https://wikitech.wikimedia.org/wiki/Add_Link/Datasets for details" > "$DATASET_PUBLISH_PATH"/README
