cc7b642739725deb43e601196c63627a5cdc0f4d
Kosta Harlan <kharlan@wikimedia.org>
2021-03-17 12:59:03 +0100

    Move gunicorn configuration to conf file, adjust config
    Bug: T277297
    Change-Id: I7c31ac5b13cf0946197224749f66caab729db98d

---
+++ gunicorn.conf.py
@@ -0,0 +1,11 @@
+bind = "0.0.0.0:8000"
+# Emit events to sidecar container in production.
+statsd_host = "localhost:9125"
+statsd_prefix = "linkrecommendation"
+# See https://docs.gunicorn.org/en/stable/design.html#how-many-workers which
+# suggests the formula (2 x $num_cores) + 1
+# Conservatively assuming 2 cores
+workers = 5
+# Processing larger pages is slow.
+timeout = 60
+graceful_timeout = 60
