5e164507306073425d662783c54142f1ae8c8b96
Kosta Harlan <kharlan@wikimedia.org>
2022-05-09 13:21:31 +0200

    tests: Add integration testing setup script
    Bug: T303515
    Change-Id: Ibf9ae53476d3b84649c09b0297794444f967408e

---
+++ .pipeline/integration.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Download datasets for simplewiki (the smallest datasets we have) and place them
+# in the data directory. These will then be used by pytest integration tests.
+
+WIKI_ID=${WIKI_ID:-simplewiki}
+echo "Creating directory for data/$WIKI_ID and switching to it"
+# In CI, home directory is the same as app dir, but that's probably not the case
+# in your local environment.
+APP_DIR=$(pwd)
+mkdir -p "$APP_DIR/data"
+mkdir -p "$APP_DIR/data/$WIKI_ID"
+cd "$APP_DIR/data/$WIKI_ID"
+
+echo "Downloading and verifying linkmodel..."
+curl -Os https://analytics.wikimedia.org/published/datasets/one-off/research-mwaddlink/$WIKI_ID/$WIKI_ID.linkmodel.json.checksum
+curl -Os https://analytics.wikimedia.org/published/datasets/one-off/research-mwaddlink/$WIKI_ID/$WIKI_ID.linkmodel.json
+shasum -a 256 -c $WIKI_ID.linkmodel.json.checksum
+
+download_and_verify_sqlite() {
+  echo "Downloading and verifying $1 and $2..."
+  curl -Os https://analytics.wikimedia.org/published/datasets/one-off/research-mwaddlink/$WIKI_ID/$1
+  curl -Os https://analytics.wikimedia.org/published/datasets/one-off/research-mwaddlink/$WIKI_ID/$2
+  # Get rid of the relative path to the checksum
+  CHECKSUM=$(cat $1| cut -d ' ' -f1)
+  echo "$CHECKSUM  $2" > $1
+  shasum -a 256 -c $1
+  gunzip --force $2
+}
+
+CHECKSUM_FILENAME=$WIKI_ID.anchors.sqlite.checksum
+FILENAME=$WIKI_ID.anchors.sqlite.gz
+download_and_verify_sqlite $CHECKSUM_FILENAME $FILENAME
+
+CHECKSUM_FILENAME=$WIKI_ID.pageids.sqlite.checksum
+FILENAME=$WIKI_ID.pageids.sqlite.gz
+download_and_verify_sqlite $CHECKSUM_FILENAME $FILENAME
+
+CHECKSUM_FILENAME=$WIKI_ID.redirects.sqlite.checksum
+FILENAME=$WIKI_ID.redirects.sqlite.gz
+download_and_verify_sqlite $CHECKSUM_FILENAME $FILENAME
+
+CHECKSUM_FILENAME=$WIKI_ID.w2vfiltered.sqlite.checksum
+FILENAME=$WIKI_ID.w2vfiltered.sqlite.gz
+download_and_verify_sqlite $CHECKSUM_FILENAME $FILENAME
+
+# Back to app root
+cd "$APP_DIR"
