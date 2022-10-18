8b645e0cec5fb032f48d9d712d2fad747e283d93
Martin Urbanec <martin.urbanec@wikimedia.cz>
2022-10-18 17:21:46 +0200

    MediaWikiApi: Follow redirects manually
    requests seems to bypass proxy when it follows redirect.
    Use allow_redirects=False and follow the redirect manually
    instead (using the proxy).
    
    Refactors request-making to MediaWikiApi._make_request,
    so it can be reused in other methods than MediaWikiApi.get_article,
    if-needed.
    
    Bug: T321082
    Change-Id: I4f4beb6b434aa95729b7da9e87de4d871001bbde

--- src/MediaWikiApi.py
+++ src/MediaWikiApi.py
@@ -1,4 +1,5 @@
 import requests
+from urllib.parse import urlparse
 
 
 class MediaWikiApi:
@@ -13,20 +14,55 @@ class MediaWikiApi:
         :param wiki_domain The wiki domain to use with queries, e.g. "en" for English Wikipedia.
           Current assumption is that it is a Wikipedia wiki ID,
           non-Wikipedia wiki IDs are not yet supported.
-        :param api_url The URL to use for connecting to MediaWiki REST API, it should contain the full path to rest.php,
-          e.g. http://localhost:8080/w/rest.php
-        :param proxy_api_url: The proxy URL to use for connecting to MediaWiki API. In production it is a value
-          like http://localhost:6500/w/rest.php
+        :param api_url The base URL to use for connecting to MediaWiki, it should contain the path,
+          to the wiki's root directory, e.g. http://localhost:8080/w/
+        :param proxy_api_url: The proxy URL to use for network requests (only scheme and network location
+          is used). In production it is a value like http://localhost:6500.
         :param project The project to use for the request, e.g. "wikipedia" or "wiktionary"
         """
-        self.api_url = api_url
         self.proxy_api_url = proxy_api_url
         self.project = project
         # Ensure that wiki IDs like bat_smg are converted to bat-smg for domain resolution.
         self.wiki_domain = wiki_domain.replace("_", "-")
-        # In production, API queries should go to the proxy URL
-        if self.proxy_api_url:
-            self.api_url = self.proxy_api_url
+
+        self.api_url = api_url or "https://%s.%s.org/w/" % (
+            self.wiki_domain,
+            self.project,
+        )
+
+    def _make_request(
+        self, method: str, url: str, *args, **kwargs
+    ) -> requests.Response:
+        kwargs["allow_redirects"] = False
+
+        if "headers" not in kwargs:
+            kwargs["headers"] = {}
+        kwargs["headers"]["User-Agent"] = "linkrecommendation"
+
+        redirects_followed = 0
+        while True:
+            # In production, API queries should go to the proxy URL
+            if self.proxy_api_url:
+                parsed_url = urlparse(url)
+                parsed_proxy = urlparse(self.proxy_api_url)
+
+                kwargs["headers"]["Host"] = parsed_url.netloc
+
+                url = parsed_url._replace(
+                    scheme=parsed_proxy.scheme, netloc=parsed_proxy.netloc
+                ).geturl()
+
+            r = requests.request(method, url, *args, **kwargs)
+            if not r.is_redirect:
+                break
+
+            url = r.headers["location"]
+            redirects_followed += 1
+
+            if redirects_followed > 10:
+                raise Exception("Too many redirects")
+
+        return r
 
     def get_article(self, title: str, revision: int = None) -> dict:
         """
@@ -34,15 +70,6 @@ class MediaWikiApi:
         :param title The page title, not urlencoded.
         :param revision Page revision (defaults to latest)
         """
-
-        # Use the API url if specified via an environment variable or
-        # the production API endpoint; both of these are developer
-        # setup configurations.
-        if not self.proxy_api_url:
-            self.api_url = self.api_url or "https://%s.%s.org/w/" % (
-                self.wiki_domain,
-                self.project,
-            )
 
         request_params = {
             "action": "query",
@@ -58,15 +85,8 @@ class MediaWikiApi:
             request_params["titles"] = title
             request_params["rvlimit"] = 1
 
-        headers = {
-            "User-Agent": "linkrecommendation",
-        }
-        # If we have a proxy URL then we should use the Host header
-        if self.proxy_api_url:
-            headers["Host"] = "%s.%s.org" % (self.wiki_domain, self.project)
-
-        response = requests.get(
-            self.api_url + "api.php", headers=headers, params=request_params
+        response = self._make_request(
+            "GET", self.api_url + "api.php", params=request_params
         )
         return self.make_response(response.json())
 
--- tests/test_MediaWikiApi.py
+++ tests/test_MediaWikiApi.py
@@ -1,18 +1,17 @@
 from src.MediaWikiApi import MediaWikiApi
 import requests_mock
+import pytest
 
 
 def test_init():
-    mw_api = MediaWikiApi(
-        wiki_domain="cs", api_url="https://api/", proxy_api_url="https://proxy_api/"
-    )
-    assert mw_api.api_url == "https://proxy_api/"
-
     mw_api = MediaWikiApi(wiki_domain="cs", api_url="https://api/")
     assert mw_api.api_url == "https://api/"
 
     mw_api = MediaWikiApi(wiki_domain="bat_smg", api_url="https://api/")
     assert mw_api.wiki_domain == "bat-smg"
+
+    mw_api = MediaWikiApi(wiki_domain="en")
+    assert mw_api.api_url == "https://en.wikipedia.org/w/"
 
 
 def test_get_article():
@@ -20,6 +19,22 @@ def test_get_article():
     with requests_mock.Mocker() as m:
         m.get(
             "https://api/api.php?action=query&prop=revisions&titles=Lipsko",
+            json=get_default_response(),
+        )
+        assert mw_api.get_article("Lipsko") == {
+            "wikitext": "Foo",
+            "pageid": 12345,
+            "revid": 56789,
+        }
+
+
+def test_get_article_proxy():
+    mw_api = MediaWikiApi(
+        wiki_domain="cs", api_url="https://api/", proxy_api_url="https://proxy_api/"
+    )
+    with requests_mock.Mocker() as m:
+        m.get(
+            "https://proxy_api/api.php?action=query&prop=revisions&titles=Lipsko",
             json=get_default_response(),
         )
         assert mw_api.get_article("Lipsko") == {
@@ -49,11 +64,24 @@ def test_host_headers():
     )
     with requests_mock.Mocker() as m:
         m.get(
-            "https://proxy/api.php",
+            "https://proxy/w/api.php",
             json=get_default_response(),
             request_headers={"Host": "cs.wikipedia.org"},
         )
         mw_api.get_article(title="Lipsko")
+
+
+def test_max_redirects():
+    mw_api = MediaWikiApi(wiki_domain="cs", api_url="http://api/")
+    with requests_mock.Mocker() as m:
+        m.get(
+            "http://api/api.php", status_code=301, headers={"Location": "https://foo"}
+        )
+        m.get("https://foo", status_code=301, headers={"Location": "https://bar"})
+        m.get("https://bar", status_code=301, headers={"Location": "https://foo"})
+
+        with pytest.raises(Exception):
+            mw_api.get_article(title="Lipsko")
 
 
 def get_default_response() -> dict:
