89e1eb6ca17533bb12f040821466e57e8159fe44
Martin Urbanec <martin.urbanec@wikimedia.cz>
2023-08-22 19:36:52 +0200

    publish-datasets: Require exact match in wikis.txt to skip list update
    Why:
    Certain wiki IDs contain each other ("gomwiki" and "omwiki" are
    both valid wiki IDs). For those projects, wikis.txt was not updated,
    leading in missing those wikis when the linkrecommendation k8s service
    processes the generated data.
    
    What:
    Instead of using only $WIKI as the search pattern for grep,
    prefix it with ^ (start of line) and $ (end of line) to require
    an exact match, rather than a substring search.
    
    Bug: T340944
    Change-Id: Ibaffee6d904bca490f4d39f32108e508c78575f2

--- publish-datasets.sh
+++ publish-datasets.sh
@@ -17,7 +17,7 @@ cp "$DATASET_PATH/"*.checksum "$DATASET_PUBLISH_PATH/"
 # Write to an index of all wikis that have published datasets. This file is parsed by
 # load-datasets.py when a specific wiki ID is not defined and the --download option
 # is used.
-if ! grep -q "$WIKI_ID" "$DATASET_LISTING_FILE"; then
+if ! grep -q "^$WIKI_ID$" "$DATASET_LISTING_FILE"; then
   echo "$WIKI_ID" >> $DATASET_LISTING_FILE
 fi
 
